359b9f968bce525a4e962a259ae0d56d9a294249
tuanhadev <tuanitvip99@gmail.com>
2020-08-10 15:25:10 +0700

    init project

---
+++ src/containers/_nav.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Dashboard',
+    to: '/dashboard',
+    icon: 'cil-speedometer',
+    badge: {
+      color: 'info',
+      text: 'NEW',
+    }
+  }
+]
+
