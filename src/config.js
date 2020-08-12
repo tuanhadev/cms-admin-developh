aac7d4e2fd12e7cf9792eea00e9b314f59ba5c3c
tuanhadev <tuanitvip99@gmail.com>
2020-08-12 16:26:58 +0700

    config deploy gitthub page

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-    domain: "http://localhost:8080",
+    // http://localhost:8080
+    domain: "https://damp-river-04072.herokuapp.com",
     METHOD: {
         GET: 'get',
         POST: 'post',
